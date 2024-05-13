ed61a46cf9e90b6b60da03f9daccf00b3ff4d3ba
hk1996yhx <50992362+hk1996yhx@users.noreply.github.com>
2024-05-13 23:26:08 +0800

    Update once.sh

--- once.sh
+++ once.sh
@@ -8,4 +8,5 @@ apt update
 apt install -y nano  vim curl wget git
 apt update
 apt install -y   neofetch tmate
+curl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py > /usr/bin/systemctl && chmod +x /usr/bin/systemctl
 echo "-: Package and Sudo setup complete. :-"
