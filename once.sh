fee0d721426c8983c303265532e1a14a6b0a7d46
hk1996yhx <50992362+hk1996yhx@users.noreply.github.com>
2024-05-13 22:56:12 +0800

    Create once.sh

---
+++ once.sh
@@ -0,0 +1,11 @@
+apt update
+apt upgrade -y
+apt update --fix-missing
+apt install -y sudo
+apt update
+apt install -y apt-utils 
+apt update
+apt install -y nano  vim curl wget git
+apt update
+apt install -y   neofetch tmate
+echo "-: Package and Sudo setup complete. :-"
