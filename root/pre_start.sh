79bef45a5948d5f0266e8b70066dc9bc683ffa43
hk1996yhx <50992362+hk1996yhx@users.noreply.github.com>
2024-06-01 17:21:41 +0800

    Update pre_start.sh

--- root/pre_start.sh
+++ root/pre_start.sh
@@ -1,4 +1,2 @@
 su
-apt update && apt upgrade -y && apt update --fix-missing && apt install neofetch nano git vim curl wget sudo tmate  -y
-curl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py > /usr/bin/systemctl && chmod +x /usr/bin/systemctl
-echo "-: Package and Sudo setup complete. :-"
+apt update && apt upgrade -y 
