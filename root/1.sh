37dd1274004135b5d01827057d25d650e4a6870e
hk1996yhx <50992362+hk1996yhx@users.noreply.github.com>
2024-06-01 18:33:07 +0800

    Update 1.sh

--- root/1.sh
+++ root/1.sh
@@ -1,8 +1,8 @@
 apt update
 apt install -y sudo
 apt install -y locales
-locale-gen en_US.UTF-8
-update-locale LANG=en_US.UTF-8
+locale-gen zh_CN.UTF-8
+update-locale LANG=zh_CN.UTF-8
 apt install -y dialog apt-utils
 apt update && apt upgrade -y
 apt install -y neofetch nano git vim curl wget sudo tmate && neofetch
