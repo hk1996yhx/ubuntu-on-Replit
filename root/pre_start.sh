1ec93036459f205ad57a42ac2fb1b6240ce2d540
hk1996yhx <50992362+hk1996yhx@users.noreply.github.com>
2024-06-01 17:42:12 +0800

    Update pre_start.sh

--- root/pre_start.sh
+++ root/pre_start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-echo "Running pre-start script..."
 # 在这里添加你想要运行的命令
-apt update && apt upgrade -y 
+apt update
+apt install sudo
+sudo apt-get install locales
+sudo locale-gen en_US.UTF-8
+sudo update-locale LANG=en_US.UTF-8
